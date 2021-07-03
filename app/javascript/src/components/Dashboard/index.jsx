1f26f479c071fb798b2452acbbb60984fb211ce7
amaldinesh7 <amalkdinesh@gmail.com>
2021-07-03 22:19:41 +0530

    Create new note pane added

--- app/javascript/src/components/Dashboard/index.jsx
+++ app/javascript/src/components/Dashboard/index.jsx
@@ -6,6 +6,8 @@ import Navbar from "components/Common/Navbar";
 import PasswordEdit from "./Account/Passwords/Edit";
 import Profile from "./Account/Profile";
 import Notes from "./Notes";
+import Contacts from "./Contacts";
+import Settings from "./Settings";
 
 const Home = () => {
   return (
@@ -14,6 +16,8 @@ const Home = () => {
       <div className="flex flex-col items-start justify-start flex-grow h-screen overflow-y-auto">
         <Switch>
           <Route exact path="/notes" component={Notes} />
+          <Route exact path="/contacts" component={Contacts} />
+          <Route exact path="/settings" component={Settings} />
           <Route exact path="/my/password/edit" component={PasswordEdit} />
           <Route exact path="/my/profile" component={Profile} />
           <Redirect from="/" to="/notes" />
